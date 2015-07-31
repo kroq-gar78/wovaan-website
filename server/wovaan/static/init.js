b13ad9717d17ca3f302f6c09937193a44cd9a347
Ritvik <ritu6ritu@gmail.com>
2015-07-31 17:28:08 -0500

    e

--- server/wovaan/static/init.js
+++ server/wovaan/static/init.js
@@ -30,12 +30,12 @@ function fetchScramble()
 {
   $.ajax({
     type: "POST",
-    url: "/updatescramble/",
+    url: "http://127.0.0.1:8000/updatescramble/", // TODO: change URL when moved into django
     datatype: "html",
     data: "",
     success: function(result)
     {
-      $("#scramble").html(result);
+      $("#scrambles").html(result);
     }
   });
 }
