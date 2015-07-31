41028d24692a7f2d23938a596221ba3968eb2d46
Ritvik <ritu6ritu@gmail.com>
2015-07-31 16:38:57 -0500

    Static Files WIP

--- server/wovaan/urls.py
+++ server/wovaan/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.conf.urls.static import static
 
 from wovaan.scrambler import scramble_cube
 
